97f60c1bc56c1d34ce92678a007e2f61f1cc5666
Xorufake <85679972+Fadhlan88888@users.noreply.github.com>
2024-07-24 21:01:37 +0700

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,8 +1,8 @@
 <?php
-$databaseHost = 'localhost';
-$databaseName = 'test';
-$databaseUsername = 'root';
-$databasePassword = 'root';
+$databaseHost = 'db4free.net';
+$databaseName = 'fadhlan77';
+$databaseUsername = 'fadhlan77';
+$databasePassword = 'fadhlan77';
 
 // Open a new connection to the MySQL server
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
